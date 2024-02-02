a59e8f0fd626c89cbe054fa9b7b567003397b0aa
E K <ekamalov967@gmail.com>
2024-02-02 14:11:38 +0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ export default async ({ req, res, log, error }) => {
      .setEndpoint('https://cloud.appwrite.io/v1')
      .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
      .setKey(process.env.APPWRITE_API_KEY);
-
+const database = new sdk.Databases(client);
   await database.createDocument('65532fcadc03be880396', '65bca37ff23142ac5902', sdk.ID.unique(), {time: (+new Date()).toString()});
 
     res.json({'success': true});
